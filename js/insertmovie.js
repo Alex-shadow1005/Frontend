441d54293c80bd6ca47d154e4e04e99b423dfb46
Nicklas00 <nick553h@stud.kea.dk>
2022-03-15 12:11:40 +0100

    HTML oprettet index og til at oprette ny film i database. JS også oprettet til indsæt film

---
+++ js/insertmovie.js
@@ -0,0 +1,59 @@
+const out = function (str) {
+    console.log(str);
+}
+out("hej");
+
+document.addEventListener('DOMContentLoaded', createFormEventListener);
+
+let countyForm;
+function createFormEventListener() {
+    countyForm = document.getElementById("newMovieForm");
+    countyForm.addEventListener('submit', handleFormSubmit);
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    out("hej1");
+    const form = event.currentTarget;
+    const url = form.action;
+    out(form);
+    out(url);
+    try {
+        const formData = new FormData(form);
+        out(formData);
+        const responseData = await postFormDataAsJson(url, formData);
+        out(responseData);
+        alert(formData.get('filmname') + ' er oprettet');
+
+    } catch (err) {
+        alert(err.message);
+        out(err);
+    }
+}
+
+async function postFormDataAsJson(url, formData) {
+    out(formData.entries());
+    const plainFormData = Object.fromEntries(formData.entries());
+    out("xxxx");
+    out(plainFormData);
+    /*
+    plainFormData.filmname = {};
+    plainFormData.region.regionCode = "1081";
+    */
+
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: formDataJsonString
+    };
+
+    const response = await fetch(url, fetchOptions);
+    if (!response) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+    console.log(response.json);
+    return response.json();
+}
