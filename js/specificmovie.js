e8e8f2e35f2e561652ac5d3622e5e0e237c83e5b
Zerkia <lordzerkia@gmail.com>
2022-03-25 12:26:54 +0100

    specific movie logic complete, css WIP

--- js/specificmovie.js
+++ js/specificmovie.js
@@ -5,13 +5,44 @@ console.log(id);
 const divContainer = document.getElementById("specificmovie");
 
 async function loadSpecificMovie() {
-    console.log(divContainer);
-    const specificMovie = await fetchSpecificMovie("http://localhost:8181/movies/" + id);
-    const showMovieName = document.createElement("h1");
-    showMovieName.type = "text";
-    showMovieName.setAttribute("value", specificMovie.movie_name);
+    const specificMovie = await fetchSpecificMovie("http://localhost:8181/movie/" + id);
 
-    divContainer.appendChild(specificMovie);
+    //image_link
+    const showMovieImage = document.createElement('img');
+    showMovieImage.classList.add("image_link");
+    showMovieImage.src = specificMovie.image_link;
+    divContainer.appendChild(showMovieImage);
+
+    //movie_name
+    const showMovieName = document.createElement('h1');
+    showMovieName.classList.add("movie_name");
+    showMovieName.innerText = specificMovie.movie_name;
+    divContainer.appendChild(showMovieName);
+
+    //movie_length
+    const showMovieLength = document.createElement('h1');
+    showMovieLength.innerText = specificMovie.movie_length;
+    divContainer.appendChild(showMovieLength);
+
+    //description
+    const showMovieDescription = document.createElement('h1');
+    showMovieDescription.innerText = specificMovie.description;
+    divContainer.appendChild(showMovieDescription);
+
+    //genre
+    const showMovieGenre = document.createElement('h1');
+    showMovieGenre.innerText = specificMovie.genre;
+    divContainer.appendChild(showMovieGenre);
+
+    //pg_rating
+    const showMovieRating = document.createElement('h1');
+    showMovieRating.innerText = specificMovie.pg_rating;
+    divContainer.appendChild(showMovieRating);
+
+    //release_date
+    const showMovieReleaseDate = document.createElement('h1');
+    showMovieReleaseDate.innerText = specificMovie.release_date;
+    divContainer.appendChild(showMovieReleaseDate);
 }
 
 function fetchSpecificMovie(url){
